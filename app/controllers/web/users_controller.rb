58f16f4942a0016975a43ea3a70d9f9f91947b03
Heimdell <hindmost.one@gmail.com>
2012-09-13 16:17:29 +0400

    Removed junk comments before methods in UsersController

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -1,7 +1,5 @@
 class Web::UsersController < Web::ProtectedController
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.
         order("name").
@@ -9,25 +7,18 @@ class Web::UsersController < Web::ProtectedController
         per(params[:per_page])
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = UserEditType.new(params[:user])
 
@@ -38,8 +29,6 @@ class Web::UsersController < Web::ProtectedController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
   def update
     @user = UserEditType.find(params[:id])
 
@@ -50,12 +39,11 @@ class Web::UsersController < Web::ProtectedController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
     @user.destroy
 
       redirect_to users_path
   end
+
 end
